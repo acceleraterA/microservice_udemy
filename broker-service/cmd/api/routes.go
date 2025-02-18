f176bb87b2553a508cd5bfeb45844baace7ee960
acceleraterA <mzga@ucdavis.edu>
2025-02-18 12:20:30 -0800

    l25 broker connect to authentication service

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -22,5 +22,7 @@ func (app *Config) routes() http.Handler {
 	mux.Use(middleware.Heartbeat("/ping"))
 	// route to broker service
 	mux.Post("/", app.Broker)
+	mux.Post("/handle", app.HandleSubmission)
+
 	return mux
 }
