2ac3dbd751e3859ba6b8718e6b96526a0c998128
acceleraterA <mzga@ucdavis.edu>
2025-02-18 10:03:01 -0800

    l24 route and handler to accept json

---
+++ authentication-service/cmd/api/handlers.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	// read the json to requestPayload
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	//validate the user against the database
+	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		return
+	}
+	valid, err := user.PasswordMatches(requestPayload.Password)
+	if err != nil || !valid { // if error or password does not match
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		return
+	}
+	payload := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("Logged in user %s", user.Email),
+		Data:    user,
+	}
+	app.writeJSON(w, http.StatusAccepted, payload)
+
+}
