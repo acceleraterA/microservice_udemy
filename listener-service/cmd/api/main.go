e6ece0fd8b5a4a4e9fc930d99e2d019f65e5868e
acceleraterA <mzga@ucdavis.edu>
2025-02-23 20:35:32 -0800

    l49 create listener service

---
+++ listener-service/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	//try to connect to rabbitmq
+
+	// start listening for msg
+
+	// create consumer
+
+	// watch the queue and consume events
+}
